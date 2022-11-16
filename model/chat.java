9514172dadeb89e3e89d46b73f8b73095ac10257
ThomLooij <tja.vanderlooij@student.avans.nl>
2022-11-16 16:38:49 +0100

    commit

--- model/chat.java
+++ model/chat.java
@@ -1,14 +1,25 @@
 package model;
 
 public class chat {
+    private DatabaseConnector dbConn;
     private int gameid;
-    private int playerid;
     private String messageString;
     private String timeStamp;
 
-    public chat(int gameid){
+    public chat(int gameid, DBConn dbConn){
+        this.dbConn = dbConn;
         this.gameid = gameid;
     }
 
-    public String 
+    public String getMessageString(){
+        return messageString;
+    }
+
+    public String getTimeStamp(){
+        return timeStamp;
+    }
+
+    public String showText(){
+        if ()
+    }
 }
